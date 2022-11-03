4656ef4a53c63f1049418fe34f9a9c160ca4ce08
Sabin Subedi <sabinsubedi125@gmail.com>
2022-11-03 18:19:45 +0545

    nubir

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path 
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path("", views.dashboard, name="signup"),
-    path("login/", views.login, name="login"),
+    path("login/", views.login.as_view(), name="login"),
     path("signup/", views.signup, name="signup"),
     path('forgot_password/', views.forgot_password, name="forgot_password"),
 ]
-
--- user/views.py
+++ user/views.py
@@ -1,15 +1,59 @@
-from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseRedirect
-
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic import CreateView, TemplateView
+from .forms import LoginForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 # Create your views here.
-def login(request):
-    return render(request,'login.html')
+
 
 def forgot_password(request):
-    return render(request,'forgot_password.html')
+    return render(request, 'forgot_password.html')
+
 
 def signup(request):
-    return render(request,'signup.html')
+    form = LoginForm()
+    print(form.fields.get('password').widget.attrs)
+    return render(request, 'login.html')
+
 
 def dashboard(request):
     return HttpResponseRedirect(redirect_to="/login")
+
+
+class login(CreateView):
+    form_class = LoginForm
+    template_name = "login.html"
+    http_method_names= ['post','get']
+    
+    def get(self,request,*args,**kwargs):
+        print(request)
+        return super().post(request, *args, **kwargs)
+    
+    def post(self,request,*args,**kwargs):
+        print(request)
+        return super().post(request, *args, **kwargs)
+
+
+# def login(request):
+#     form =LoginForm()
+#     context = {
+#         "form": form
+#     }
+    
+#     if request.method == "POST":
+#         form = LoginForm(request.POST)
+        
+        
+#         if form.is_valid():
+#             data = form.cleaned_data
+#             user = authenticate(request, email=data.get('email'), password=data.get('email'))
+            
+
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect("home")
+#         else:
+#             print(context)
+#             return render(request, "login.html",{"form":form})
+#     return render(request, "login.html",context)
